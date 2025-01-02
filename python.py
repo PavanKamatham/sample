5f705fc8a5f130fb38172129eaae65607546d671
PavanKamatham <37448368+PavanKamatham@users.noreply.github.com>
2025-01-01 22:27:20 -0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,9 @@
+def main(a,b):
+ print ("The sum of ",a," and ", b ," is : ",a+b)
+
+ try:
+  a = int(input("Enter value of a:"))
+  b = int(input("Enter value of b:"))
+  main(a,b)
+ expect:
+ print("Value Should be an integer")
